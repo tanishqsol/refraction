fca2c2a936ad017d8439ab1e4fc49f7a45a0f7d8
tanishqsolanki <tanishqsolanki7@gmail.com>
2021-08-17 16:01:31 +0530

    click changes div color

--- src/app/station-page/input-selection/input-selection.component.ts
+++ src/app/station-page/input-selection/input-selection.component.ts
@@ -9,6 +9,10 @@ import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 export class InputSelectionComponent implements OnInit {
   isTenChosen: boolean = false;
   positionOfPower: any;
+  selected: any;
+  selectedZeroToTen: any;
+  selectedPlusMinus: any;
+  selectedDecimal: any;
   constructor(@Inject(MAT_DIALOG_DATA) public data: any) { }
   targetValue;
   defaultLoad = [];
@@ -17,9 +21,9 @@ export class InputSelectionComponent implements OnInit {
   targetValueAnswer1Sphere: any = "";
   targetValueAnswer1Cylinder: any = "";
   targetValueAnswer1Axis: any = "";
-  isPlusMinusChosen: boolean = false;
-  isZeroToTenChosen: boolean = false;
-  isDecimalChosen: boolean = false;
+  isPlusMinusChosen;
+  isZeroToTenChosen;
+  isDecimalChosen;
   plusMinus = ["+", "-"]
   zeroToTen = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
   decimalFloat = [".00", ".25", ".50", ".75"]
@@ -36,18 +40,18 @@ export class InputSelectionComponent implements OnInit {
     console.log(event)
     this.defaultLoad[1] = val
     this.targetValue = this.defaultLoad[0] + this.defaultLoad[1] + this.defaultLoad[2]
-    this.isZeroToTenChosen = event.checked ? true : false
+    this.selectedZeroToTen = val;
   }
   appendPlusMinus(val) {
     this.defaultLoad[0] = val
     this.targetValue = this.defaultLoad[0] + this.defaultLoad[1] + this.defaultLoad[2]
-    this.isPlusMinusChosen = true;
+    this.selectedPlusMinus = val;
 
   }
   appendDecimal(val) {
     this.defaultLoad[2] = val
     this.targetValue = this.defaultLoad[0] + this.defaultLoad[1] + this.defaultLoad[2]
-    this.isDecimalChosen = true;
+    this.selectedDecimal = val;
   }
 
   setAppropriatePowerValue() {
